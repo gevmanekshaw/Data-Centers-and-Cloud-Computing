61daefc32f17ba08524544c315424af21717ac74
gevmanekshaw <gkm240@nyu.edu>
2016-04-02 22:40:51 -0400

    Create topo2_1.py

---
+++ 4_Mininet_Toplogy_with_Manual_Flow_Rules/topo2_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+
+
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+def emptyNet():
+
+    "Create an empty network and add nodes to it."
+
+    net = Mininet( controller=Controller )
+
+    info( '*** Adding controller\n' )
+    net.addController( 'c0' )
+
+    info( '*** Adding hosts\n' )
+    h1 = net.addHost( 'h1', ip='10.0.0.1' )
+    h2 = net.addHost( 'h2', ip='10.0.0.2' )
+
+    info( '*** Adding switch\n' )
+    s1 = net.addSwitch( 's1' )
+    s2 = net.addSwitch( 's2' )
+    s3 = net.addSwitch( 's3' )
+    s4 = net.addSwitch( 's4')
+
+    info( '*** Creating links\n' )
+    net.addLink( h1, s1 )
+    net.addLink( h2, s4 )
+    net.addLink( s1, s2 )
+    net.addLink( s2, s4 )
+    net.addLink( s3, s4 )
+    net.addLink( s1, s3 )
+
+    info( '*** Starting network\n')
+    net.start()
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    emptyNet()
